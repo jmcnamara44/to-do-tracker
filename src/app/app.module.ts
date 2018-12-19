babbce56920bb46bf08fc6eb125158945c2ff32b
Jimmy McNamara <jmcnamara44@gmail.com>
2018-12-19 13:47:23 -0800

    initial commit with header and create-activity components created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NewActivityComponent } from './new-activity/new-activity.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NewActivityComponent
   ],
   imports: [
     BrowserModule
