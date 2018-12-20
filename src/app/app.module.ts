26c8ae2e894d55a7a6459a588ac9c7968a64eb32
Jimmy McNamara <jmcnamara44@gmail.com>
2018-12-19 23:32:50 -0800

    create an activity list with sample values.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NewActivityComponent } from './new-activity/new-activity.component';
+import { ActivityListComponent } from './activity-list/activity-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NewActivityComponent
+    NewActivityComponent,
+    ActivityListComponent
   ],
   imports: [
     BrowserModule
