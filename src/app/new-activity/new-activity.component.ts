de9e0febd685628ba1de1ac8e1f016d2530c43a8
Jimmy McNamara <jmcnamara44@gmail.com>
2018-12-20 23:30:48 -0800

    create a form for the user to create a new activity to track.

--- src/app/new-activity/new-activity.component.ts
+++ src/app/new-activity/new-activity.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-new-activity',
   templateUrl: './new-activity.component.html',
   styleUrls: ['./new-activity.component.css']
 })
-export class NewActivityComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class NewActivityComponent {
+  formSelected = null;
+  showForm() {
+    this.formSelected = true;
   }
-
 }
