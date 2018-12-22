96d62dcb04e484092982626882a58f6958811f48
Jimmy McNamara <jmcnamara44@gmail.com>
2018-12-22 15:22:40 -0800

    bring the user to a new page when an activity is clicked on. move activity-list selector out of app html and into the header html.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { ActivityDetailComponent } from './activity-detail/activity-detail.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: HeaderComponent
+  },
+  {
+    path: 'details',
+    component: ActivityDetailComponent
   }
 ];
 
