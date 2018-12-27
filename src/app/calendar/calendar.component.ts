e1126d1b89be0e6d37eba5e72574f46ed4d68de0
Jimmy McNamara <jmcnamara44@gmail.com>
2018-12-26 22:21:51 -0800

    add calendar dates for december and style formats.

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -7,16 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
 
+  // months: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-
-  buildMonthlyCalendar(){
-    var currentMonth: number = new Date().getMonth() + 1;
-    var currentYear: number = new Date().getFullYear();
-    var dateString: string = currentMonth + '/1/' + currentYear;
-    var firstDayOfMonth: Date = new Date(dateString);
-    var weekdayOfFirstDayOfMonth: number = firstDayOfMonth.getDay();
-
-  }
+  // buildMonthlyCalendar(){
+  //   var currentMonth: number = new Date().getMonth() + 1;
+  //   var currentYear: number = new Date().getFullYear();
+  //   var dateString: string = currentMonth + '/1/' + currentYear;
+  //   var firstDayOfMonth: Date = new Date(dateString);
+  //   var weekdayOfFirstDayOfMonth: number = firstDayOfMonth.getDay();
+  //   if()
+  //
+  // }
 
   constructor() { }
 
