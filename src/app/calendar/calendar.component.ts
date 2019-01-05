a958f4296af52687acce52912b1f1d66e29bb02f
Jimmy McNamara <jmcnamara44@gmail.com>
2019-01-04 23:03:33 -0800

    create a calendar on the activity detail page.

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -6,22 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
+  today = new Date();
+  currentMonth = this.today.getMonth();
+  currentYear = this.today.getFullYear();
+  selectYear = document.getElementById("year");
+  selectMonth = document.getElementById("month");
 
-  // months: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-  // buildMonthlyCalendar(){
-  //   var currentMonth: number = new Date().getMonth() + 1;
-  //   var currentYear: number = new Date().getFullYear();
-  //   var dateString: string = currentMonth + '/1/' + currentYear;
-  //   var firstDayOfMonth: Date = new Date(dateString);
-  //   var weekdayOfFirstDayOfMonth: number = firstDayOfMonth.getDay();
-  //   if()
-  //
-  // }
+  months: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
   constructor() { }
 
   ngOnInit() {
+    this.showCalendar(this.currentMonth, this.currentYear);
+  }
+
+  showCalendar(month, year) {
+    let firstDay = (new Date(year, month)).getDay();
+    let daysInMonth = 32 - new Date(year, month, 32).getDate();
+    let tbl = document.getElementById("calendar-body");
+    document.getElementById("month-name").innerHTML = this.months[month] + " " + year;
+    this.selectYear = year;
+    this.selectMonth = month;
+
+    let date = 1;
+    for(let i = 0; i < 6; i++){
+      let row = document.createElement("tr");
+
+      for(let j = 0; j < 7; j++){
+        if(i === 0 && j < firstDay) {
+          let cell = document.createElement("td");
+          let cellText = document.createTextNode("");
+          cell.appendChild(cellText);
+          row.appendChild(cell);
+        } else if(date > daysInMonth) {
+          break;
+        } else {
+          let cell = document.createElement("td");
+          let dateString = date.toString();
+          let cellText = document.createTextNode(dateString);
+          if(date === this.today.getDate() && year === this.today.getFullYear() && month === this.today.getMonth()) {
+            cell.classList.add("red");
+          }
+          cell.appendChild(cellText);
+          row.appendChild(cell);
+          date++;
+        }
+      }
+      tbl.appendChild(row);
+    }
+
   }
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  showFormClicked = null;
+
+  showFormClicked = false;
   startShowForm() {
     this.showFormClicked = true;
   }
