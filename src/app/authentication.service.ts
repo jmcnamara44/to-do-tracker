92f6bdf09dc20b8aaa8e2091e4cbfeb677121da5
Jimmy McNamara <jmcnamara44@gmail.com>
2019-02-21 12:34:02 -0800

    use the username as the key in the db when an account is created.

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -12,17 +12,23 @@ export class AuthenticationService {
   }
   login(userName: string, password: string) {
       //checks to see if the username exists, if it doesnt then a message appears showing the user this. if it does then it inserts the password into the hashing function and sees if this matches the hash code that is originall associated with the username already saved
+      //what is the most efficient way to search for and see if a user exists in our database?
+      // this.user = database.object()
   }
 
   logout() {
 
   }
-  createUser(userName: string, password: string) {
+  createUser(usersName: string, password: string) {
     //the code to create a username and password. we will add a new user to the database, possibly using a hash table in order to make looking up a user that much easier when we totally have like a billion users.
     let passwordHash = this.passwordHashFunction(password);
     let activities = [];
-    let newUser: User = new User(userName, passwordHash);
-    this.users.push(newUser);
+    let newUser: User = new User(usersName, passwordHash);
+    this.database.object('/users/' + usersName).set({
+      userName: usersName,
+      passwordHash: passwordHash,
+      // activities: []
+    })
   }
 
   userNameHashFunction(userName: string) {
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from './../authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [AuthenticationService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
   }
   login(userName: string, password: string) {
     //checks to see if the username exists, if it doesnt then a message appears showing the user this. if it does then it inserts the password into the hashing function and sees if this matches the hash code that is originall associated with the username already saved
+    this.authenticationService.login(userName, password);
   }
 }
