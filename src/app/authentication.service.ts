e2e75297c232b1ede84b100dc23b3ea0c6970e5a
Jimmy McNamara <jmcnamara44@gmail.com>
2019-02-22 12:59:52 -0800

    retrieve user information if user enters a valid login credentials.

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -5,15 +5,32 @@ import { User } from './models/user.model';
 @Injectable()
 export class AuthenticationService {
   user;
+  userLoggedIn: boolean = false;
   users: FirebaseListObservable<any[]>;
   constructor(private database: AngularFireDatabase)
   {
     this.users = database.list('users');
   }
+
+  getUserByUserName(userName: string) {
+    return this.database.object('users/' + userName);
+  }
+
   login(userName: string, password: string) {
       //checks to see if the username exists, if it doesnt then a message appears showing the user this. if it does then it inserts the password into the hashing function and sees if this matches the hash code that is originall associated with the username already saved
       //what is the most efficient way to search for and see if a user exists in our database?
       // this.user = database.object()
+      let passwordHash = this.passwordHashFunction(password);
+      this.getUserByUserName(userName).subscribe(dataLastEmittedFromObserver => {
+        this.user = dataLastEmittedFromObserver;
+        console.log(this.user);
+        if (passwordHash != this.user.passwordHash) {
+          this.user = null;
+          console.log("nahhhhh");
+        }
+        this.userLoggedIn = true;
+        console.log(this.user);
+      });
   }
 
   logout() {
@@ -23,7 +40,7 @@ export class AuthenticationService {
     //the code to create a username and password. we will add a new user to the database, possibly using a hash table in order to make looking up a user that much easier when we totally have like a billion users.
     let passwordHash = this.passwordHashFunction(password);
     let activities = [];
-    let newUser: User = new User(usersName, passwordHash);
+    let newUser: User = new User(usersName, 4);
     this.database.object('/users/' + usersName).set({
       userName: usersName,
       passwordHash: passwordHash,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,7 +13,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
   }
-  login(userName: string, password: string) {
+  beginLogin(userName: string, password: string) {
     //checks to see if the username exists, if it doesnt then a message appears showing the user this. if it does then it inserts the password into the hashing function and sees if this matches the hash code that is originall associated with the username already saved
     this.authenticationService.login(userName, password);
   }
