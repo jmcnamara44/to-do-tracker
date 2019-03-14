a0046835972ec72dad17d496ffd1cd25f459bb4e
Jimmy McNamara <jmcnamara44@gmail.com>
2019-03-14 02:57:49 -0700

    create a link for the user to return to their page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from './authentication.service';
+import { Router } from '@angular/router';
+import { UserServiceService } from './user-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [AuthenticationService]
+  providers: [AuthenticationService, UserServiceService]
 })
 
 export class AppComponent {
-
-  constructor(private authenticationService: AuthenticationService) { }
+  userName: string;
+  constructor(private router: Router, private userService: UserServiceService, private authenticationService: AuthenticationService) { }
 
   beginLogout() {
     this.authenticationService.logout();
+    this.router.navigate(['']);
+  }
+
+  goToUserHome() {
+    this.authenticationService.user.subscribe(value => {
+      if (value !== null) {
+        this.userName = value.email;
+        this.userService.loginUser(value.uid, value.email);
+      }
+    })
   }
 }
